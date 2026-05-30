45e9a112a40f885b546a13ab5986b9135900098c
Mikael-arch <mikael.matias@estudante.ifms.edu.br>
2026-05-30 12:22:59 -0400

    second

--- include/quicksort.h
+++ include/quicksort.h
@@ -2,7 +2,7 @@
 #define QUICKSORT_H
 #include <stdio.h>
 
-void quicksort_crescente(int *vetor, int inicial, int final);
+void quicksort(int *vetor, int inicial, int final);
 int particionar(int *vetor, int inicial, int final);
 
 #endif
